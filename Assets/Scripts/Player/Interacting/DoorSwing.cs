ab0cbefc4002396d8c7167636220137f3d37a8e2
Rory Rouillon <118494211+SirSmoothie@users.noreply.github.com>
2025-04-25 18:16:19 +1000

    Changed the door swing timing

--- Assets/Scripts/Player/Interacting/DoorSwing.cs
+++ Assets/Scripts/Player/Interacting/DoorSwing.cs
@@ -5,7 +5,7 @@ public class DoorSwing : MonoBehaviour, IInteractable
 {
     public bool isOpen = false;
     public float swingAngle = 90f;
-    public float swingDuration = 0.5f;
+    public float swingDuration = 2f;
     public Vector3 hingeAxis = Vector3.up;
 
     private Quaternion closedRotation;
