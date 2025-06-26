f930cf3243346da07fbb69dbfa81ee95f7508fc7
Rory Rouillon <118494211+SirSmoothie@users.noreply.github.com>
2025-06-26 22:38:17 +1000

    DayandNightCycle now matches with the time

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -19,40 +19,97 @@ public class GameManager : MonoBehaviour
     }
     #endregion
 
-    public float degreePerSecond;
-    public Vector3 rotation;
+    private float degreePerSecond;
+    private Vector3 rotation;
     public GameObject sun;
-    public int worldTimeMinutes;
-    public int worldTimeHours = 7;
-    public int worldTimeDays;
+    public float worldTimeSeconds;
+    public float worldTimeMinutes;
+    public float worldTimeHours = 7;
+    public float worldTimeDays;
     private float _timer;
-    
-    
+    private float WorldClockUpdater;
+
+    public float secondsInADay;
+    public float minutesInADay;
+    public float hoursInADay;
+    private float _totalSecondsInADay;
+    public Vector3 _timePerSecond;
+
+    private void Start()
+    {
+        _totalSecondsInADay = secondsInADay + minutesInADay*60 + hoursInADay*3600;
+        CalculateDayCycleDegrees();
+        CalculateTimePerSecond();
+    }
+
+    #region Day and Night Cycle Calculations
+
+    private void CalculateDayCycleDegrees()
+    {
+        degreePerSecond = 360/_totalSecondsInADay;
+    }
+
+    private void CalculateTimePerSecond()
+    {
+        int tempRoundedMins = 0;
+        int tempRoundedHours = 0;
+        float tempSeconds = 0f;
+        float tempMinutes = 0f;
+        float tempHours = 0f;
+        
+         tempSeconds = 86400/_totalSecondsInADay;
+         tempMinutes = tempSeconds/60;
+        tempRoundedMins = Mathf.FloorToInt(tempMinutes);
+        tempSeconds -= tempRoundedMins*60;
+        if (tempRoundedMins >= 60)
+        {
+            tempHours = tempRoundedMins / 60;
+            tempRoundedHours = Mathf.FloorToInt(tempHours);
+            tempRoundedMins -= tempRoundedHours * 60;
+        }
+        
+        _timePerSecond = new Vector3(tempSeconds, tempRoundedMins, tempRoundedHours);
+    }
+
+    #endregion
+
     private void Update()
     {
         rotation.x = degreePerSecond * Time.deltaTime;
         sun.transform.Rotate(rotation,Space.World);
         _timer += Time.deltaTime;
-        if (_timer >= 1f)
+        WorldClockUpdater += Time.deltaTime;
+        if (WorldClockUpdater >= 1)
         {
-            
-            worldTimeMinutes += 6;
+            worldTimeSeconds += _timePerSecond.x;
+            if (worldTimeSeconds >= 60)
+            {
+                worldTimeSeconds -= 60;
+                worldTimeMinutes++;
+            }
+            worldTimeMinutes += _timePerSecond.y;
             if (worldTimeMinutes >= 60)
             {
                 worldTimeMinutes -= 60;
-                worldTimeHours += 1;
-                if (worldTimeHours >= 24)
-                {
-                    worldTimeHours -= 24;
-                    worldTimeDays += 1;
-                }
+                worldTimeHours++;
             }
-            _timer -= 1;
+            worldTimeHours += _timePerSecond.z;
+            if (worldTimeHours >= 24)
+            {
+                worldTimeHours -= 24;
+                worldTimeDays++;
+            }
+            WorldClockUpdater -= 1;
+        }
+        if (_timer >= _totalSecondsInADay)
+        {
+            _timer = 0f;
         }
     }
 
     public void SetTime(Vector3 time)
     {
+        //Dont Use this fuction, its dumb and only changes the time on the clock not the position of the sun
         worldTimeMinutes = Mathf.RoundToInt(time.x);
         worldTimeHours = Mathf.RoundToInt(time.y);
         worldTimeDays = Mathf.RoundToInt(time.z);
